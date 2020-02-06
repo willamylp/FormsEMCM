339a1e64c1ecd2c2f23af5d275ce6cc85e236dc0
ArturCarlos <arturcarlossantiago@gmail.com>
2020-02-06 09:58:23 -0300

    funcão marcar Chekbox

--- tela/js/script.js
+++ tela/js/script.js
@@ -6,4 +6,55 @@
             $(".CheckboxMagico").prop('checked', "");
         }
     });
+
+    $(".CaixaMagica2").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico2").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico2").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica3").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico3").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico3").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica4").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico4").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico4").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica5").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico5").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico5").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica6").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico6").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico6").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica7").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico7").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico7").prop('checked', "");
+        }
+    });
+    $(".CaixaMagica8").on("keyup", function (e) {
+        if (this.value != "") {
+            $(".CheckboxMagico8").prop("checked", "checked");
+        } else {
+            $(".CheckboxMagico8").prop('checked', "");
+        }
+    });
+
 });
