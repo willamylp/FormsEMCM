028b5b85a3754c56efab0b1e730c8be13f718490
ArturCarlos <arturcarlossantiago@gmail.com>
2020-02-06 09:18:56 -0300

    funcão marcar Chekbox

--- tela/js/script.js
+++ tela/js/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(function(){
+    $(".CaixaMagica").keyup(function(){
+        if($(this).val().length > 0){
+            $(this).parent().parent().find(".CheckboxMagico").prop("checked", true);
+        } else {
+            $(this).parent().parent().find(".CheckboxMagico").prop("checked", false);
+        }
+    });
+});
